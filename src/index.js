1c0ac7852f58ce16a183bb0d7b69ff42f4a9fab3
Tom Noogen <tomn@Toms-MacBook-Pro.local>
2017-05-08 19:01:43 -0500

    save state

---
+++ src/index.js
@@ -0,0 +1,129 @@
+import L from 'leaflet';
+import Wu from 'wu';
+require('leaflet.path.drag');
+require('leaflet-editable');
+
+const wu = new Wu();
+
+/**
+ * Leaflet Image Mapper
+ */
+export default class Limapper {
+  /**
+   * initialize object
+   * @return {object} Instance
+   */
+  constructor() {
+    this._name = 'Limapper';
+    this._items = [];
+  }
+
+  /**
+   * get name
+   * @return {string} name
+   */
+  get name() {
+    return this._name;
+  }
+
+  /**
+   * get items
+   * @return {Array} list of items
+   */
+  get items() {
+    if (!this._map) {
+      return [];
+    }
+
+    let items = [];
+    let map = this._map;
+    var origin = map.latLngToLayerPoint(new L.LatLng(0, 0));
+
+    wu.each(this._items, (v, k) => {
+      v.pxpoints = [];
+      wu.each(v._latlngs[0], (p, k2) =>{
+        let p2 = map.latLngToLayerPoint(p);
+        v.pxpoints.push({x: p2.x - origin.x, y: p2.y - origin.y });
+      });
+
+      items.push(v);
+    });
+
+    return items;
+  }
+
+  /**
+   * initialize object
+   * @param  {object} opts options
+   * @return {object}      self
+   */
+  init(opts) {
+    let self = this;
+    let defs = {
+      minZoom: 1,
+      maxZoom: 5,
+      center: [0, 0],
+      zoom: 1,
+      editable: true,
+      crs: L.CRS.Simple
+    };
+    let southWest, northEast, bounds, map, pointOrigin;
+
+    opts = wu.defaults(opts, defs);
+    map = L.map(opts.elid || 'map', opts);
+    southWest = map.unproject([0, opts.imageHeight]);
+    northEast = map.unproject([opts.imageWidth, 0]);
+    bounds = new L.LatLngBounds(southWest, northEast);
+    L.imageOverlay(opts.imageUrl, bounds).addTo(map);
+    map.setMaxBounds(bounds);
+    this._map = map;
+
+    L.EditControl = L.Control.extend({
+      options: {
+        position: 'topleft',
+        callback: null,
+        kind: '',
+        html: ''
+      },
+      onAdd: function (map) {
+        let container = L.DomUtil.create('div', 'leaflet-control leaflet-bar'),
+          link = L.DomUtil.create('a', '', container);
+
+        link.href = '#';
+        link.title = 'Create a new ' + this.options.kind;
+        link.innerHTML = this.options.html;
+        L.DomEvent
+          .on(link, 'click', L.DomEvent.stop)
+          .on(link, 'click', function () {
+            window.LAYER = this.options.callback.call(map.editTools);
+        }, this);
+
+        return container;
+      }
+    });
+
+    map.on('layeradd', function(e) {
+
+      if (e.layer instanceof L.Path) {
+        self._items.push(e.layer);
+        setTimeout(function() {
+          self.items();
+
+        }, 100);
+      }
+    });
+
+    L.NewRectangleControl = L.EditControl.extend({
+      options: {
+        position: 'topleft',
+        callback: map.editTools.startRectangle,
+        kind: 'rectangle',
+        html: '⬛'
+      }
+    });
+
+    map.addControl(new L.NewRectangleControl());
+
+    return self;
+  }
+}
